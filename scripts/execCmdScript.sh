e51d518ac63fbed6a9bd41d82550b132a5438247
alexey.lazarenko <dzotic9@gmail.com>
2020-04-07 15:26:18 +0300

    JE-52445

--- scripts/execCmdScript.sh
+++ scripts/execCmdScript.sh
@@ -29,8 +29,8 @@ if [ "${1}" == "slave" ]; then
     sed -i "s|.*max_wal_senders.*|max_wal_senders = 1|g" ${PGSQL_DATA}postgresql.conf;
     sed -i "s|.*archive_mode.*|archive_mode = on|g" ${PGSQL_DATA}postgresql.conf;
     sed -i "s|.*archive_command.*| archive_command = 'cd .'|g" ${PGSQL_DATA}postgresql.conf;
-    echo "standby_mode = on" > ${PGSQL_DATA}recovery.conf;
-    echo "primary_conninfo = 'host=${MASTER_IP} port=5432 user=replication password=${DB_PASSWORD}'" >> ${PGSQL_DATA}recovery.conf;
-    echo "trigger_file = '/tmp/postgresql.trigger.5432'" >> ${PGSQL_DATA}recovery.conf;
+    echo "standby_mode = on" >> ${PGSQL_DATA}postgresql.conf;
+    echo "primary_conninfo = 'host=${MASTER_IP} port=5432 user=replication password=${DB_PASSWORD}'" >> ${PGSQL_DATA}postgresql.conf;
+    echo "trigger_file = '/tmp/postgresql.trigger.5432'" >> ${PGSQL_DATA}postgresql.conf;
     sudo /etc/init.d/postgresql start
 fi
